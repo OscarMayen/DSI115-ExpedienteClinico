8f969dffdd98016a7afe606bc63b0f16251421df
Edwin Diaz <josephph1993@gmail.com>
2019-12-02 09:34:53 -0600

    Se han agregado los reporte de usuarios, recetas y consultas atendidas

--- src/java/com/managedbean/reporteManagedBean.java
+++ src/java/com/managedbean/reporteManagedBean.java
@@ -5,25 +5,39 @@
  */
 package com.managedbean;
 
+import com.ejb.MedicoEJB;
+import com.ejb.PacienteEJB;
+import com.entities.MedicoEntity;
+import com.entities.PacienteEntity;
 import edu.utilidades.invocarReporte;
 import java.io.Serializable;
 import java.sql.SQLException;
-import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
 import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
+import javax.inject.Named;
 import javax.servlet.ServletContext;
 
 /**
  *
  * @author josue
  */
-@ManagedBean(name = "reporteManagedBean")
+@Named(value = "reporteManagedBean")
 @ViewScoped
 public class reporteManagedBean implements Serializable{
 
-    private Integer idPaciente;
-    
-    
+ @EJB
+   private PacienteEJB pacienteEJB;   
+   @EJB 
+   private MedicoEJB medicoEJB;
+   private Integer idPaciente;
+   private PacienteEntity paciente;
+   private MedicoEntity medico;
+   List<PacienteEntity> pacientes=new ArrayList<PacienteEntity>();
+   List<MedicoEntity> medicos=new ArrayList<MedicoEntity>();
+
     public reporteManagedBean() {
     }
 
@@ -49,5 +63,113 @@ public class reporteManagedBean implements Serializable{
         r.getReporte(ruta);        
         FacesContext.getCurrentInstance().responseComplete();               
     }
+     
+       public void verReporteRecetas() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
+//         Instancia hacia la clase reporteClientes   
+           System.out.println(paciente.getIdPaciente());
+           System.out.println(medico.getIdMedico());
+        invocarReporte r = new invocarReporte();
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();
+        
+        if(paciente!=null&&medico!=null){
+        String ruta = servletContext.getRealPath("/admin/reportes/ReporteRecetasParametros.jasper");                
+        r.getReporteRecetasParametros(ruta, paciente, medico);            
+        FacesContext.getCurrentInstance().responseComplete();    
+        }
+        else{
+        String ruta = servletContext.getRealPath("/admin/reportes/ReporteRecetasNoParametros.jasper");                
+        r.getReporteRecetas(ruta);
+        FacesContext.getCurrentInstance().responseComplete();    
+        }
+        
+                   
+     }
+     
+     
+     public void verReporteReceta(Integer id) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
+//         Instancia hacia la clase reporteClientes        
+        invocarReporte r = new invocarReporte();
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();                
+        String ruta = servletContext.getRealPath("/admin/reportes/ReporteReceta.jasper");                
+        r.getReporteReceta(ruta,id);            
+        FacesContext.getCurrentInstance().responseComplete();    
+       
+     }
+     public void verReporteConsultasAtendidas() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
+//         Instancia hacia la clase reporteClientes        
+        invocarReporte r = new invocarReporte();
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();                
+        String ruta = servletContext.getRealPath("/admin/reportes/ReporteConsultasPorMedico.jasper");                
+        r.getReporteConsultasAtendidas(ruta);
+        FacesContext.getCurrentInstance().responseComplete();    
+       
+     }
+     public void verReporteUsuarios() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
+//         Instancia hacia la clase reporteClientes        
+        invocarReporte r = new invocarReporte();
+        
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ServletContext servletContext = (ServletContext) facesContext.getExternalContext().getContext();                
+        String ruta = servletContext.getRealPath("/admin/reportes/ReporteDeUsuarios.jasper");                
+        r.getReporteDeUsuarios(ruta);
+        FacesContext.getCurrentInstance().responseComplete();    
+       
+     }
+     
+     
+    public void obtenerPaciente(int id) {
+        System.out.println(id);
+        paciente=pacienteEJB.obtenerPaciente(id);
+        
+    }
+    
+    public void obtenerMedico(int id) {        
+        System.out.println(id);        
+        this.medico=medicoEJB.obtenerMedico(id);     
+        
+    }
+    
+     
+     public PacienteEntity getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(PacienteEntity paciente) {
+        this.paciente = paciente;
+    }
+
+    public MedicoEntity getMedico() {
+        medicos=medicoEJB.listarMedico();
+        return medico;
+    }
+
+    public void setMedico(MedicoEntity medico) {
+        this.medico = medico;
+    }
+
+    public List<PacienteEntity> getPacientes() {
+        pacientes=pacienteEJB.listarPaciente();
+        return pacientes;
+    }
+
+    public void setPacientes(List<PacienteEntity> pacientes) {
+        this.pacientes = pacientes;
+    }
+
+    public List<MedicoEntity> getMedicos() {
+        medicos=medicoEJB.listarMedico();
+        return medicos;
+    }
+
+    public void setMedicos(List<MedicoEntity> medicos) {
+        this.medicos = medicos;
+    }
+       
     
 }
--- src/java/edu/utilidades/invocarReporte.java
+++ src/java/edu/utilidades/invocarReporte.java
@@ -5,6 +5,8 @@
  */
 package edu.utilidades;
 
+import com.entities.MedicoEntity;
+import com.entities.PacienteEntity;
 import java.io.*;
 import java.sql.*;
 import java.util.*;
@@ -164,6 +166,252 @@ public class invocarReporte {
                 }
             }
         }
+    }
+     
+     
+      public void getReporteRecetasParametros(String ruta, PacienteEntity paciente,MedicoEntity medico) throws  ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Connection conexion;
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/expedienteclinico", "root", "grupo10");
+
+        //Se definen los parametros si es que el reporte necesita
+        Map parameter = new HashMap();
+        parameter.put("idPaciente", paciente.getIdPaciente());
+        parameter.put("idMedico", medico.getIdMedico());
+        
+
+
+        try {
+            File file = new File(ruta);
+
+            HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+
+            httpServletResponse.setContentType("application/pdf");
+            httpServletResponse.addHeader("Content-Type", "application/pdf");
+
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(file.getPath());
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameter, conexion);
+
+            JRExporter jrExporter = null;                      
+            jrExporter = new JRPdfExporter();
+            jrExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            jrExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, httpServletResponse.getOutputStream());
+
+            if (jrExporter != null) {
+                try {
+                    jrExporter.exportReport();
+                } catch (JRException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }}
+        
+        public void getReporteRecetas(String ruta) throws  ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Connection conexion;
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/expedienteclinico", "root", "grupo10");
+
+        //Se definen los parametros si es que el reporte necesita
+        Map parameter = new HashMap();        
+
+
+        try {
+            File file = new File(ruta);
+
+            HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+
+            httpServletResponse.setContentType("application/pdf");
+            httpServletResponse.addHeader("Content-Type", "application/pdf");
+
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(file.getPath());
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameter, conexion);
+
+            JRExporter jrExporter = null;                      
+            jrExporter = new JRPdfExporter();
+            jrExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            jrExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, httpServletResponse.getOutputStream());
+
+            if (jrExporter != null) {
+                try {
+                    jrExporter.exportReport();
+                } catch (JRException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        
+    }
+        
+        public void getReporteReceta(String ruta, Integer id) throws  ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Connection conexion;
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/expedienteclinico", "root", "grupo10");
+
+        //Se definen los parametros si es que el reporte necesita
+        Map parameter = new HashMap(); 
+        parameter.put("idReceta", id);
+
+
+        try {
+            File file = new File(ruta);
+
+            HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+
+            httpServletResponse.setContentType("application/pdf");
+            httpServletResponse.addHeader("Content-Type", "application/pdf");
+
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(file.getPath());
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameter, conexion);
+
+            JRExporter jrExporter = null;                      
+            jrExporter = new JRPdfExporter();
+            jrExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            jrExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, httpServletResponse.getOutputStream());
+
+            if (jrExporter != null) {
+                try {
+                    jrExporter.exportReport();
+                } catch (JRException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        
+    }
+        
+        public void getReporteConsultasAtendidas(String ruta) throws  ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Connection conexion;
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/expedienteclinico", "root", "grupo10");
+
+        //Se definen los parametros si es que el reporte necesita
+        Map parameter = new HashMap(); 
+        
+
+
+        try {
+            File file = new File(ruta);
+
+            HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+
+            httpServletResponse.setContentType("application/pdf");
+            httpServletResponse.addHeader("Content-Type", "application/pdf");
+
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(file.getPath());
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameter, conexion);
+
+            JRExporter jrExporter = null;                      
+            jrExporter = new JRPdfExporter();
+            jrExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            jrExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, httpServletResponse.getOutputStream());
+
+            if (jrExporter != null) {
+                try {
+                    jrExporter.exportReport();
+                } catch (JRException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        
+    }
+        public void getReporteDeUsuarios(String ruta) throws  ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        Connection conexion;
+        Class.forName("com.mysql.jdbc.Driver").newInstance();
+        conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/expedienteclinico", "root", "grupo10");
+
+        //Se definen los parametros si es que el reporte necesita
+        Map parameter = new HashMap(); 
+        
+        try {
+            File file = new File(ruta);
+
+            HttpServletResponse httpServletResponse = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+
+            httpServletResponse.setContentType("application/pdf");
+            httpServletResponse.addHeader("Content-Type", "application/pdf");
+
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObjectFromFile(file.getPath());
+
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameter, conexion);
+
+            JRExporter jrExporter = null;                      
+            jrExporter = new JRPdfExporter();
+            jrExporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            jrExporter.setParameter(JRExporterParameter.OUTPUT_STREAM, httpServletResponse.getOutputStream());
+
+            if (jrExporter != null) {
+                try {
+                    jrExporter.exportReport();
+                } catch (JRException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        
+        
     }
 }
    
